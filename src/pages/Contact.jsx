d2f7ce48750715c7b22afb973327102ae5ce59af
Jeniffer Giraldo <tris@Jeniffers-MacBook-Air-2.local>
2023-07-04 13:32:49 -0400

    FIRST commit, router practice

---
+++ src/pages/Contact.jsx
@@ -0,0 +1,11 @@
+import NavList from "../components/NavList";
+
+export default function Contact () {
+
+    return (
+        <>
+         <h2>Contact</h2>
+        <NavList />
+        </>
+    )
+}
---
+++ src/pages/Err404.jsx
@@ -0,0 +1,13 @@
+import NavList from "../components/NavList";
+
+export default function Error404() {
+
+    return (
+        <>
+        <h2>Error 404</h2>
+        <NavList />
+        <p>You reached the end of the internet.</p>
+        <p>Use nav to go back.</p>
+        </>
+    )
+}
